fd107a3c213bafca0af70b3e4458e050151a530d
Shivers1093 <brandon@smttreecare.com>
2026-04-27 10:51:32 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "AI Construstion Estimator",
+  description: "AI estimate generator",
+};
+export default function Rootlayout({ children }) {
+  return (
+    <html lang="en">
+    <body>{children}</body>
+    </html>
+  );
+}
